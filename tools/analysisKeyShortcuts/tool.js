01f6db5607ed9609dd964312e7938e1231a8bb4b
Siderite <Costin.Manda@gmail.com>
2023-07-01 00:09:49 +0300

    Implemented key move modes. . (dot) then a digit will play the nth move in the variations list. Ctrl-. then a digit will play the nth computer move. Shift-. then a digit will play the nth explorer move. https://github.com/Siderite/lichessTools/issues/32

--- tools/analysisKeyShortcuts/tool.js
+++ tools/analysisKeyShortcuts/tool.js
@@ -22,6 +22,82 @@
       }
     }
 
+    clearMoveMode=()=>{
+      const g=this.lichessTools.global;
+      g.clearTimeout(this.makeMoveTimeout);
+      this.makeMoveMode=null;
+      $('body').removeClass('lichessTools-keyMode-pgn');
+      $('body').removeClass('lichessTools-keyMode-ceval');
+      $('body').removeClass('lichessTools-keyMode-explorer');
+    };
+
+    prepareMove=(mode)=>{
+      const g=this.lichessTools.global;
+      this.clearMoveMode();
+      switch(mode) {
+        case 'pgn': {
+          const moves=$('div.analyse__tools div.analyse__fork move');
+          if (!moves.length) return;
+        }
+        break;
+        case 'ceval': {
+          const analysis=this.lichessTools.lichess.analysis;
+          if (!analysis?.ceval || !analysis?.ceval.enabled()) return;	
+        }
+        break;
+        case 'explorer': {
+          const moves=$('.explorer-box:not(.loading) .moves tbody tr');
+          if (!moves.length) return;
+        }
+        break;
+        default:
+          g.console.warn('Unknown key move mode',mode);
+          return;
+      }
+      this.makeMoveMode=mode;
+      $('body').addClass('lichessTools-keyMode-'+mode);
+      this.makeMoveTimeout=g.setTimeout(this.clearMoveMode,1500);
+    };
+
+    handleDigitKey=(combo)=>{
+      const g=this.lichessTools.global;
+      let index=+combo;
+      if (!index) return;
+      index--;
+      if (!this.makeMoveMode && this.oldHandlers[combo]) {
+        this.oldHandlers[combo]();
+        return;
+      }
+      switch(this.makeMoveMode) {
+        case 'pgn': {
+          const moves=$('div.analyse__tools div.analyse__fork move');
+          const move=moves[index];
+          move?.click();
+        }
+        break;
+        case 'ceval': {
+          const analysis=this.lichessTools.lichess.analysis;
+          if (!analysis?.ceval || !analysis?.ceval.enabled()) return;	
+          const pvs=analysis?.node?.ceval?.pvs;
+          if (!pvs||!pvs[index]) return;
+          const uci=pvs[index].moves[0];
+          if (uci) analysis.playUci(uci);
+        }
+        break;
+        case 'explorer': {
+          const analysis=this.lichessTools.lichess.analysis;
+          if (!analysis) return;
+          const moves=$('.explorer-box:not(.loading) .moves tbody tr');
+          const uci=moves.eq(index).attr('data-uci');
+          if (uci) analysis.playUci(uci);
+        }
+        break;
+        default:
+          g.console.warn('Unknown key move mode',this.makeMoveMode);
+          return;
+      }
+    };
+
     async start() {
       const parent=this.lichessTools;
       const value=parent.currentOptions.keyShortcuts;
@@ -45,6 +121,16 @@
       parent.unbindKeyHandler('alt+m',true);
       parent.unbindKeyHandler('alt+b',true);
 
+      parent.unbindKeyHandler('.',true);
+      parent.unbindKeyHandler('ctrl+.',true);
+      parent.unbindKeyHandler('shift+.',true);
+
+      for (let i = 1; i <=9 ; i++) {
+        const combo=i.toString();
+        if (!this.oldHandlers[combo]) this.oldHandlers[combo]=parent.getKeyHandler(combo);
+        parent.unbindKeyHandler(combo);
+      }
+
       if (value) {
         parent.bindKeyHandler('i',()=>parent.jumpToGlyphSymbol(currentPlayer,'?!'));
         parent.bindKeyHandler('m',()=>parent.jumpToGlyphSymbol(currentPlayer,'?'));
@@ -52,11 +138,23 @@
         parent.bindKeyHandler('alt+i',()=>parent.jumpToGlyphSymbol(otherPlayer,'?!'));
         parent.bindKeyHandler('alt+m',()=>parent.jumpToGlyphSymbol(otherPlayer,'?'));
         parent.bindKeyHandler('alt+b',()=>parent.jumpToGlyphSymbol(otherPlayer,'??'));
+
+        parent.bindKeyHandler('.',()=>this.prepareMove('pgn'));
+        parent.bindKeyHandler('ctrl+.',()=>this.prepareMove('ceval'));
+        parent.bindKeyHandler('shift+.',()=>this.prepareMove('explorer'));
+        for (let i = 1; i <=9 ; i++) {
+          const combo=i.toString();
+          parent.bindKeyHandler(combo,()=>this.handleDigitKey(combo));
+        }
       } else {
         if (this.oldHandlers) {
           parent.bindKeyHandler('i',this.oldHandlers['i'],true);
           parent.bindKeyHandler('m',this.oldHandlers['m'],true);
           parent.bindKeyHandler('b',this.oldHandlers['b'],true);
+          for (let i = 1; i <=9 ; i++) {
+            const combo=i.toString();
+            if (this.oldHandlers[combo]) parent.bindKeyHandler(combo,this.oldHandlers[combo],true);
+          }
         }
       }
     }
