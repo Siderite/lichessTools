85c780591299e74562d2ef0434155b192c434083
Siderite <Costin.Manda@gmail.com>
2023-07-15 23:49:03 +0300

    Adding tool to show uncommon glyphs https://github.com/Siderite/lichessTools/issues/9

--- scripts/lichessTools-init.js
+++ scripts/lichessTools-init.js
@@ -40,6 +40,7 @@ lichessTools.loadTool(LiChessTools.Tools.ExplorerPractice);
 lichessTools.loadTool(LiChessTools.Tools.SaveOptionsForIncognito);
 lichessTools.loadTool(LiChessTools.Tools.ChapterClearArtifacts);
 lichessTools.loadTool(LiChessTools.Tools.CustomChatButtons);
+lichessTools.loadTool(LiChessTools.Tools.AdditionalGlyphs);
 lichessTools.loadTool(LiChessTools.Tools.UserTvHistory);
 
 lichessTools.loadTool(LiChessTools.Tools.Preferences);
