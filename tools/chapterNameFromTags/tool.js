2e90945242379e7d11fe0f6ea4ab8833b0e1edd6
Siderite <Costin.Manda@gmail.com>
2023-09-16 11:55:25 +0300

    Enter should not activate the rename button in chapter edit form https://github.com/Siderite/lichessTools/issues/209

--- tools/chapterNameFromTags/tool.js
+++ tools/chapterNameFromTags/tool.js
@@ -56,7 +56,7 @@
       }
       if (white?.trim()&&black?.trim()) {
         const text=white+' - '+black;
-        const namesButton=$('<button class="button button-empty">')
+        const namesButton=$('<button type="button" class="button button-empty">')
           .addClass('lichessTools-changeNameButton')
           .text(trans.pluralSame('changeNameToText',text))
           .attr('title',trans.noarg('changeNameToTitle'))
