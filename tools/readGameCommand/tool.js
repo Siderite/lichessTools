f157ee8f5fbfebd40d440811961e358e3156c188
Siderite <Costin.Manda@gmail.com>
2023-11-19 12:34:51 +0200

    updated readgame command

--- tools/readGameCommand/tool.js
+++ tools/readGameCommand/tool.js
@@ -15,16 +15,16 @@
       }
     ];
 
-    defaultSpeed=2000;
+    defaultSpeed=100;
 
     intl={
       'en-US':{
         'options.readGameCommand': 'Command: read game moves from here',
-        'readGameCommand.helpText': '/readgame [speed]\r\n default speed = '+this.defaultSpeed+'\r\n Esc to stop\r\nRead game moves from here'
+        'readGameCommand.helpText': '/readgame [speed] [voice]\r\n default speed = '+this.defaultSpeed+', voice=0\r\n Esc to stop\r\nRead game moves from here'
       },
       'ro-RO':{
         'options.readGameCommand': 'Comand\u0103: arat\u0103 valoare capcan\u0103 pentru pozi\u0163ie',
-        'readGameCommand.helpText': '/readgame [vitez\u0103]\r\n viteza standard = '+this.defaultSpeed+'\r\n Esc pentru stop\r\nCite\u015Fte mut\u0103rile de aici'
+        'readGameCommand.helpText': '/readgame [vitez\u0103] [voce]\r\n viteza standard = '+this.defaultSpeed+', voce=0\r\n Esc pentru stop\r\nCite\u015Fte mut\u0103rile de aici'
       }
     };
 
@@ -47,49 +47,51 @@
 
     getReadText = (node)=>{
       const parent=this.lichessTools;
-      const m=/^(?<piece>[NBRQK])?(?<start>[a-h])?([1-8])?(?<takes>x)?(?<end>[a-h][1-8])(=(?<promotion>[NBRQK]))?((?<check>\+)|(?<mate>#))?$|(?<castles>^O-O(?<long>-O)?)$/.exec(node.san);
-      let text='';
-      if (m.groups.piece) {
-        text+=this.pieces[m.groups.piece]+' ';
-      }
-      text+=m.groups.start||'';
-      if (m.groups.takes) {
-        text+=' takes ';
-      }
-      text+=m.groups.end||'';
-      if (m.groups.promotion) {
-        text+=' promotes to '+this.pieces[m.groups.promotion];
-      }
-      if (m.groups.check) {
-        text+=' check ';
-      }
-      if (m.groups.mate) {
-        text+=' mate ';
-      }
-      if (m.groups.castles) {
-        text+=' castles ';
-        if (m.groups.long) {
-          text+=' long ';
-        }
-      }
+      let text=node.san||'';
       const glyph=node.glyphs?.at(0)?.symbol;
       const glyphText=this.glyphs[glyph];
       const commentText=parent.getNodeCommentsText(node)||'';
       if (glyphText && !commentText.toLowerCase().includes(glyphText.toLowerCase())) {
-        text+=' '+glyphText+' ';
+        text+=' '+glyphText;
       }
-      text+=commentText;
-      return text?.replaceAll('.',',');
+      if (commentText) {
+        text+=', '+commentText;
+      }
+      text=text.replace(/[\r\n\.]+/g,',');
+      const r=/(?:\b(?<piece>[NBRQK])?(?<start>[a-h])?(?:[1-8])?(?<takes>x)?(?<end>[a-h][1-8])(?:=(?<promotion>[NBRQK]))?|\b(?<castles>^O-O(?<long>-O)?))(?:(?<check>\+)|(?<mate>#))?/g;
+      const pieces=this.pieces;
+      text=text.replace(r,function (...args) {
+        const groups=args.at(-1);
+        let t='';
+        if (groups.piece) {
+          t+=pieces[groups.piece]+' ';
+        }
+        t+=groups.start||'';
+        if (groups.takes) {
+          t+=' takes ';
+        }
+        t+=groups.end||'';
+        if (groups.promotion) {
+          t+=' promotes to '+pieces[groups.promotion];
+        }
+        if (groups.check) {
+          t+=' check ';
+        }
+        if (groups.mate) {
+          t+=' mate ';
+        }
+        if (groups.castles) {
+          t+=' castles ';
+          if (groups.long) {
+            t+=' long ';
+          }
+        }
+        return t;
+      });
+      return text;
     };
 
-    getDuration=(text)=>{
-      let duration=0;
-      if (!text) return duration;
-      duration=text.length/8+text.split(/\s+/).length;
-      return duration/4;
-    };
-
-    readGame = async (speed)=>{
+    readGame = async (speed,voiceIndex)=>{
       const parent=this.lichessTools;
       const $=parent.$;
       const trans=parent.translator;
@@ -101,17 +103,15 @@
       this.reading=true;
       let node=analysis.node;
       let path=analysis.path;
-      if (node.id=='') {
-        node=node.children?.at(0);
-        path+=node?.id;
-      }
       while (this.reading && node) {
         analysis.userJump(path);
         analysis.redraw();
         const text=this.getReadText(node);
-        const duration=Math.max(1,this.getDuration(text))*speed;
-        lichess.sound.say(text,false,true);
-        await parent.timeout(duration);
+        speed=+speed||0;
+        await parent.speak(text, { rate : speed?100/speed:0, voiceIndex:voiceIndex });
+        if (node!=analysis.node) {
+          this.reading=false;
+        }
         node=node.children?.at(0);
         path+=node?.id;
       }
@@ -139,10 +139,11 @@
         $('body').on('keyup',this.keyHandler);
         parent.registerCommand('readGameCommand',{
           handle:(val)=>{
-            const m=/^\s*readgame(?:\s+(\d+))?/.exec(val);
+            const m=/^\s*readgame(?:\s+(?<speed>\d+))?(?:\s+(?<voice>\d+))?/.exec(val);
             if (!m) return;
-            const speed=(+m[1])||this.defaultSpeed;
-            parent.global.setTimeout(()=>this.readGame(speed),100);
+            const speed=(+m.groups.speed)||this.defaultSpeed;
+            const voice=m.groups.voice===undefined?undefined:(+m.groups.voice)||0;
+            parent.global.setTimeout(()=>this.readGame(speed,voice),100);
             return true;
           },
           getHelp:()=>trans.noarg('readGameCommand.helpText')
