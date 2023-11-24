345f4d16280cc4f808c598e6483a93d257cf21e7
Siderite <Costin.Manda@gmail.com>
2023-11-24 10:54:18 +0200

    Atttempting to add Practice mode custom engine settings

--- tools/customEngineLevel/tool.js
+++ tools/customEngineLevel/tool.js
@@ -10,10 +10,10 @@
         advanced: true
       },
       {
-        name:'noCloud',
+        name:'customEngineOptions',
         category: 'analysis',
-        type:'single',
-        possibleValues: [false,true],
+        type:'multiple',
+        possibleValues: ['noCloud','practice'],
         defaultValue: false,
         advanced: true
       }
@@ -22,13 +22,19 @@
     intl={
       'en-US':{
         'options.analysis': 'Analysis',
-        'options.customEngineLevel': 'Minimum analysis engine depth',
-        'options.noCloud': 'Always use local engine for evaluation'
+        'options.customEngineLevel': 'Custom analysis engine depth',
+        'options.customEngineOptions': 'Custom analysis engine options',
+        'customEngineOptions.noCloud': 'Always use local engine for evaluation',
+        'customEngineOptions.practice': 'Apply in Practice mode',
+        'applyInPractice': 'Custom engine settings in Practice mode'
       },
       'ro-RO':{
         'options.analysis': 'Analiz\u0103',
-        'options.customEngineLevel': 'Nivel minim pentru motorul de analiz\u0103',
-        'options.noCloud': 'Folose\u015fte numai motorul local pentru evalu\u0103ri'
+        'options.customEngineLevel': 'Nivel personalizat pentru motorul de analiz\u0103',
+        'options.customEngineOptions': 'Op\u0163iuni motor analiz\u0103 personalizat',
+        'customEngineOptions.noCloud': 'Folose\u015fte numai motorul local pentru evalu\u0103ri',
+        'customEngineOptions.practice': 'Aplic\u0103 \u00een mod Practic\u0103',
+        'applyInPractice': 'Set\u0103ri motor personalizat \u00een mod Practic\u0103'
       }
     }
 
@@ -37,7 +43,7 @@
       const lichess=parent.lichess;
       const analysis=lichess.analysis;
       if (!analysis) return;
-      if (analysis.practice) return;
+      if (!this.options.practice && analysis.practice) return;
       let customEngineDepth=this.options.depth;
       if (!customEngineDepth && this.options.noCloud) customEngineDepth=99;
       const ceval = analysis.ceval;
@@ -55,11 +61,14 @@
       const depth = step.ceval.depth;
       if (isIdle) {
 	    if (((cloud && this.options.noCloud) || depth<customEngineDepth)) {
-          if (ceval.canGoDeeper()) {
+          if (!ceval.isDeeper() && ceval.canGoDeeper()) {
             node.autoDeeper=customEngineDepth;
             node.startDepth=step.ceval.depth;
-            ceval.goDeeper();
-            analysis.redraw();
+            setTimeout(()=>{
+              if (ceval.isDeeper() || !ceval.canGoDeeper()) return;
+              ceval.goDeeper();
+              analysis.redraw();
+            },50);
           }
         } else {
           if (ceval.isDeeper()) {
@@ -90,7 +99,48 @@
       const analysis=lichess.analysis;
       if (!analysis) return;
 
-      if (!analysis.practice) {
+      if (analysis.practice) {
+        if (this.options.practice) {
+          if (!parent.isWrappedFunction(analysis.practice.onCeval,'customEngineLevel')) {
+            analysis.practice.onCeval=parent.wrapFunction(analysis.practice.onCeval,{
+              id:'customEngineLevel',
+              before:($this,...args)=>{
+                if (this._inOnCeval||analysis.practice.isMyTurn()) return;
+                this._inOnCeval=true;
+                this._ceval=null;
+                this._node=analysis.ceval.lastStarted?.steps?.at(-1);//analysis.node
+                const ceval=this._node?.ceval;
+                if (!ceval) return;
+                if (this.options.noCloud && ceval.cloud) {
+                  this._ceval={ ...ceval };
+                  ceval.depth=1;
+                } else
+                if (this.options.depth) {
+                  if (ceval.depth>1 && ceval.depth<this.options.depth) {
+                    this._ceval={ ...ceval };
+                    ceval.depth=1;
+                  } else {
+                    const playableDepth=analysis.practice.playableDepth();
+                    if (ceval.depth>=this.options.depth && ceval.depth<playableDepth) {
+                      this._ceval={ ...ceval };
+                      ceval.depth=playableDepth;
+                    }
+                  }
+                }
+              },
+              after:($this,result,...args)=>{
+                if (this._ceval) {
+                  const ceval=this._node.ceval;
+                  ceval.depth=this._ceval.depth;
+                }
+                this._inOnCeval=false;
+              }
+            });
+          }
+        } else {
+          analysis.practice.onCeval=parent.unwrapFunction(analysis.practice.onCeval,'customEngineLevel');
+        }
+      } else {
         const customDepth=analysis.ceval?.infinite() || (analysis.ceval?.isDeeper() && !analysis.node.autoDeeper)
           ? 99
           : this.options.depth;
@@ -113,39 +163,66 @@
       $('.tview2').toggleClass('lichessTools-noCloud',this.options.noCloud);
       const container=$('div.analyse__tools div.action-menu');
       if (!container.length) return;
+
       if (!$('.abset-noCloud',container).length) {
-        const html=`<div class="setting abset-noCloud" title="LiChess Tools - $trans(options.noCloud)">
+        const html=`<div class="setting abset-noCloud" title="LiChess Tools - $trans(customEngineOptions.noCloud)">
       <div class="switch">
         <input id="abset-noCloud" class="cmn-toggle" type="checkbox" checked="">
         <label for="abset-noCloud"></label>
       </div>
-      <label for="abset-noCloud">$trans(options.noCloud)</label>
+      <label for="abset-noCloud">$trans(customEngineOptions.noCloud)</label>
     </div>`.replace(/\$trans\(([^\)]+)\)/g,m=>{
           return parent.htmlEncode(trans.noarg(m.slice(7,-1)));
         });
-        $(html).insertAfter($('div.abset-enable-nnue',container).eq(0));
+        $(html).insertAfter($('div.abset-gauge',container).eq(0));
         $('#abset-noCloud')
           .on('change',async ()=>{
             const options=parent.currentOptions;
-            options.noCloud=$('#abset-noCloud').is(':checked');
+            options.customEngineOptions=[$('#abset-noCloud').is(':checked')?'noCloud':'',this.options.practice?'practice':''].filter(o=>o).join(',');
             await parent.applyOptions(options);
             parent.fireReloadOptions();
           });
       }
       $('#abset-noCloud')
         .prop('checked',this.options.noCloud);
+
+
+      if (!$('.abset-practice',container).length) {
+        const html=`<div class="setting abset-practice" title="LiChess Tools - $trans(applyInPractice)">
+      <div class="switch">
+        <input id="abset-practice" class="cmn-toggle" type="checkbox" checked="">
+        <label for="abset-practice"></label>
+      </div>
+      <label for="abset-practice">$trans(applyInPractice)</label>
+    </div>`.replace(/\$trans\(([^\)]+)\)/g,m=>{
+          return parent.htmlEncode(trans.noarg(m.slice(7,-1)));
+        });
+        $(html).insertAfter($('div.abset-noCloud',container).eq(0));
+        $('#abset-practice')
+          .on('change',async ()=>{
+            const options=parent.currentOptions;
+            options.customEngineOptions=[this.options.noCloud?'noCloud':'',$('#abset-practice').is(':checked')?'practice':''].filter(o=>o).join(',');
+            await parent.applyOptions(options);
+            parent.fireReloadOptions();
+          });
+      }
+      $('#abset-practice')
+        .prop('checked',this.options.practice);
+
     };
 
     async start() {
       const parent=this.lichessTools;
       const value=+(parent.currentOptions.getValue('customEngineLevel'));
+      const customEngineOptions=parent.currentOptions.getValue('customEngineOptions');
       this.logOption('Custom engine level', value || 'Not set');
+      this.logOption('Custom engine options', customEngineOptions);
       this.options={
         depth: value,
-        noCloud: parent.currentOptions.getValue('noCloud'),
-        get isSet() { return this.depth || this.noCloud; }
+        noCloud: parent.isOptionSet(customEngineOptions,'noCloud'),
+        practice: parent.isOptionSet(customEngineOptions,'practice'),
+        get isSet() { return this.depth || this.noCloud || this.practice; }
       };
-      this.logOption('Do not show cloud', this.options.noCloud);
       const lichess=parent.lichess;
       const analysis=lichess.analysis;
       if (!analysis) return;
