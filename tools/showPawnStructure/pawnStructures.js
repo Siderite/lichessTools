64ccd39a9822b0aa1f84f18e99a96c3199bdbe13
Siderite <Costin.Manda@gmail.com>
2023-11-27 18:21:57 +0200

    Improvements in pawn structure feature

---
+++ tools/showPawnStructure/pawnStructures.js
@@ -0,0 +1,158 @@
+(function () {
+
+  LiChessTools.prototype.pawnStructures = {
+    '2XXX1XXT': {
+      name: 'Queen\'s Isolani',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Queen\'s_Gambit_%E2%80%93_Isolani_formation'
+    },
+    '2XXXX0TX': {
+      name: 'Italian Isolani',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Giuoco_Piano_%E2%80%93_Isolani_formation'
+    },
+    '2X2X1XTT': {
+      name: 'Hanging Pawns',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Hanging_pawns'
+    },
+    '21X2X0TM': {
+      name: 'Carlsbad',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Carlsbad_formation'
+    },
+    '21X2X1TM': {
+      name: 'Orthodox Exchange',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Carlsbad_formation'
+    },
+    'X22X21XX': {
+      name: 'Rauzer Formation',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Rauzer_formation'
+    },
+    'X221X1TM': {
+      name: 'Boleslavsky Wall',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Boleslavsky_Wall'
+    },
+    'X2011XMX': {
+      name: 'Scheveningen',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Sicilian_%E2%80%93_Scheveningen'
+    },
+    'X2010XMX 000001': {
+      name: 'Dragon',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Sicilian_%E2%80%93_Dragon'
+    },
+    'X2210XMX': {
+      name: 'Maroczi Bind',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Mar%C3%B3czy_Bind'
+    },
+    'X2201XMX': {
+      name: 'Maroczi Bind',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Mar%C3%B3czy_Bind'
+    },
+    'X2211XMX 000010': {
+      name: 'Hedgehog',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Hedgehog'
+    },
+    'X2012XMX': {
+      name: 'Boleslavsky Hole',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Boleslavsky_hole'
+    },
+    '322120XX': {
+      name: 'd5 Chain',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#d5-chain'
+    },
+    '230210XX': {
+      name: 'e5 Chain',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#e5-chain'
+    },
+    '2X321XMT': {
+      name: 'Panov',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Panov_formation'
+    },
+    '200210XX 000200': {
+      name: 'Stonewall',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Stonewall_formation'
+    },
+    '210210XX 200200': {
+      name: 'Botvinnik',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Botvinnik_system'
+    },
+    '21XX11TX': {
+      name: 'Slav',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Slav_formation'
+    },
+    '2X1X11MT': {
+      name: 'Caro',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Caro_formation'
+    },
+    '32X1X2TM': {
+      name: 'Modern Benoni',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Modern_Benoni_formation'
+    },
+    '120102XX': {
+      name: 'Closed Sicilian',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Closed_Sicilian_formation'
+    },
+    '221X02TX 0X0000': {
+      name: 'Gr\u00FCnfeld Center',
+      url: 'https://herculeschess.com/grunfeld-defense/'
+    },
+    '3X012XMT': {
+      name: 'Najdorf',
+      url: 'https://thechessworld.com/articles/openings/beating-the-najdorf-common-middlegame-structures/'
+    },
+    '3X21X2XX': {
+      name: 'Symmetric Benoni',
+      url: 'https://thechessworld.com/articles/openings/closed-center-in-kid-and-benoni/'
+    },
+    '2X320XMT': {
+      name: 'Panov II',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Panov_formation'
+    },
+    '32X12XXX': {
+      name: 'KID I',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#d5-chain'
+    },
+    '322122XX': {
+      name: 'KID II',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#d5-chain'
+    },
+    'X221X0TM': {
+      name: 'Open KID',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#Boleslavsky_Wall'
+    },
+    '222121XX': {
+      name: 'KID Complex',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#d5-chain'
+    },
+    '2X0210XM': {
+      name: 'French I',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#e5-chain'
+    },
+    'X3121XMX': {
+      name: 'French II',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#e5-chain'
+    },
+    'XX1X1XMT': {
+      name: '3-3 vs 4-2',
+      url: 'https://en.wikipedia.org/wiki/Pawn_structure#e5-chain'
+    },
+    '32X102MX 0000X1 00X0': {
+      name: 'Benko',
+      url: 'http://www.chesscorner.com/tutorial/Intermediate/openingrep/benko/benko.htm'
+    },
+    '321123XX': {
+      name: 'Closed Ruy Lopez',
+      url: 'https://chess-structures.com/2015/04/23/chess-structures-in-practice-the-closed-ruy-lopez/'
+    },
+    '221122XX': {
+      name: 'Lopez',
+      url: 'https://chess-structures.com/2015/04/23/chess-structures-in-practice-the-closed-ruy-lopez/'
+    },
+    'X20X22XX': {
+      name: 'Najdorf II',
+      url: 'https://thechessworld.com/articles/openings/beating-the-najdorf-common-middlegame-structures/'
+    },
+    'X2022XMX': {
+      name: 'Najdorf III',
+      url: 'https://thechessworld.com/articles/openings/beating-the-najdorf-common-middlegame-structures/'
+    }
+  };
+
+})();
--- tools/showPawnStructure/tool.js
+++ tools/showPawnStructure/tool.js
@@ -48,6 +48,7 @@
          'w':[false,false,false,false,false,false,false,false],
          'b':[false,false,false,false,false,false,false,false]
         },
+        islands:{ w:[], b:[] },
         qSide:{ 'w': 0, 'b': 0 },
         kSide:{ 'w': 0, 'b': 0 }
       };
@@ -70,200 +71,168 @@
           }
         }
       }
-      let w=false;
-      let b=false;
       for (let x=0; x<8; x++) {
-        if (result.pawns.w[x]>-1) {
-           if (!w && (result.qSide.w>0 || x<=3)) result.qSide.w++;
+        if (result.pawns.w[x]==-1) continue;
+        let island=result.islands.w.at(-1);
+        if (island?.end===x-1) {
+          island.end++;
         } else {
-           w=true;
-        }
-        if (result.pawns.b[x]>-1) {
-           if (!b && (result.qSide.b>0 || x<=3)) result.qSide.b++;
-        } else {
-           b=true;
+          island={ start: x, end: x };
+          result.islands.w.push(island);
         }
       }
-      w=false;
-      b=false;
-      for (let x=7; x>=0; x--) {
-        if (result.pawns.w[x]>-1) {
-           if (!w && (result.kSide.w>0 || x<=4)) result.kSide.w++;
+      for (let x=0; x<8; x++) {
+        if (result.pawns.b[x]==-1) continue;
+        let island=result.islands.b.at(-1);
+        if (island?.end===x-1) {
+          island.end++;
         } else {
-           w=true;
-        }
-        if (result.pawns.b[x]>-1) {
-           if (!b && (result.kSide.b>0 || x<=4)) result.kSide.b++;
-        } else {
-           b=true;
+          island={ start: x, end: x };
+          result.islands.b.push(island);
         }
       }
+
+      let isle=result.islands.w.at(0);
+      if (isle?.start<2) result.qSide.w=Math.min(3,isle.end)-isle.start+1;
+      isle=result.islands.b.at(0);
+      if (isle?.start<2) result.qSide.b=Math.min(3,isle.end)-isle.start+1;
+      isle=result.islands.w.at(-1);
+      if (isle?.end>5) result.kSide.w=isle.end-Math.max(4,isle.start)+1;
+      isle=result.islands.b.at(-1);
+      if (isle?.end>5) result.kSide.b=isle.end-Math.max(4,isle.start)+1;
+
       const me=blackOrientation?'b':'w';
       const they=blackOrientation?'w':'b';
       let structureText='';
       structureText+=result.pawns[me][3]<0?'X':result.pawns[me][3];
-      structureText+=result.pawns[they][3]<0?'X':result.pawns[they][3];
       structureText+=result.pawns[me][4]<0?'X':result.pawns[me][4];
-      structureText+=result.pawns[they][4]<0?'X':result.pawns[they][4];
       structureText+=result.pawns[me][2]<0?'X':result.pawns[me][2];
+      structureText+=result.pawns[they][3]<0?'X':result.pawns[they][3];
+      structureText+=result.pawns[they][4]<0?'X':result.pawns[they][4];
       structureText+=result.pawns[they][2]<0?'X':result.pawns[they][2];
       structureText+=result.qSide[me]>result.qSide[they]?'M':result.qSide[me]<result.qSide[they]?'T':'X';
       structureText+=result.kSide[me]>result.kSide[they]?'M':result.kSide[me]<result.kSide[they]?'T':'X';
       structureText+=' ';
       structureText+=result.pawns[me][5]<0?'X':result.pawns[me][5];
-      structureText+=result.pawns[they][5]<0?'X':result.pawns[they][5];
       structureText+=result.pawns[me][1]<0?'X':result.pawns[me][1];
-      structureText+=result.pawns[they][1]<0?'X':result.pawns[they][1];
       structureText+=result.pawns[me][6]<0?'X':result.pawns[me][6];
+      structureText+=result.pawns[they][5]<0?'X':result.pawns[they][5];
+      structureText+=result.pawns[they][1]<0?'X':result.pawns[they][1];
       structureText+=result.pawns[they][6]<0?'X':result.pawns[they][6];
       structureText+=' ';
       structureText+=result.pawns[me][0]<0?'X':result.pawns[me][0];
-      structureText+=result.pawns[they][0]<0?'X':result.pawns[they][0];
       structureText+=result.pawns[me][7]<0?'X':result.pawns[me][7];
+      structureText+=result.pawns[they][0]<0?'X':result.pawns[they][0];
       structureText+=result.pawns[they][7]<0?'X':result.pawns[they][7];
       structureText+=' ';
       structureText+=result.led[me][3]?'L':'X';
-      structureText+=result.led[they][3]?'L':'X';
       structureText+=result.led[me][4]?'L':'X';
-      structureText+=result.led[they][4]?'L':'X';
       structureText+=result.led[me][2]?'L':'X';
+      structureText+=result.led[they][3]?'L':'X';
+      structureText+=result.led[they][4]?'L':'X';
       structureText+=result.led[they][2]?'L':'X';
+      structureText+=' ';
       structureText+=result.led[me][5]?'L':'X';
-      structureText+=result.led[they][5]?'L':'X';
       structureText+=result.led[me][1]?'L':'X';
-      structureText+=result.led[they][1]?'L':'X';
       structureText+=result.led[me][6]?'L':'X';
+      structureText+=result.led[they][5]?'L':'X';
+      structureText+=result.led[they][1]?'L':'X';
       structureText+=result.led[they][6]?'L':'X';
+      structureText+=' ';
       structureText+=result.led[me][0]?'L':'X';
-      structureText+=result.led[they][0]?'L':'X';
       structureText+=result.led[me][7]?'L':'X';
+      structureText+=result.led[they][0]?'L':'X';
       structureText+=result.led[they][7]?'L':'X';
       return structureText;
     };
 
-    names = {
-      '2XX1XXXT': {
-        name: 'Isolani',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Queen\'s_Gambit_%E2%80%93_Isolani_formation'
-      },
-      '2XXXX0TX': {
-        name: 'Isolani',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Giuoco_Piano_%E2%80%93_Isolani_formation'
-      },
-      '2XX12XTT': {
-        name: 'Hanging Pawns',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Hanging_pawns'
-      },
-      '221XX1TM': {
-        name: 'Orthodox Exchange',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Carlsbad_formation'
-      },
-      'XX2221XX': {
-        name: 'Rauzer Formation',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Rauzer_formation'
-      },
-      'X12X21TM': {
-        name: 'Boleslavsky Wall',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Boleslavsky_Wall'
-      },
-      'X1210XMT': {
-        name: 'Scheveningen',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Sicilian_%E2%80%93_Scheveningen'
-      },
-      'X1200XMT': {
-        name: 'Dragon',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Sicilian_%E2%80%93_Dragon'
-      },
-      'X1202XMT': {
-        name: 'Maroczi Bind',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Mar%C3%B3czy_Bind'
-      },
-      'X0212XMT': {
-        name: 'Maroczi Bind',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Mar%C3%B3czy_Bind'
-      },
-      'X1212XMT': {
-        name: 'Hedgehog',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Hedgehog'
-      },
-      'X1220XMT': {
-        name: 'Boleslavsky Hole',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Boleslavsky_hole'
-      },
-      '312220XX': {
-        name: 'd5 Chain',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#d5-chain'
-      },
-      '223100XX': {
-        name: 'e5 Chain',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#e5-chain'
-      },
-      '22X13XMT': {
-        name: 'Panov',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Panov_formation'
-      },
-      '220100XX': {
-        name: 'Stonewall',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Stonewall_formation'
-      },
-      '221100XX': {
-        name: 'Botvinnik',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Botvinnik_system'
-      },
-      '2X11X1TM': {
-        name: 'Slav',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Slav_formation'
-      },
-      '2XX111MT': {
-        name: 'Caro',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Caro_formation'
-      },
-      '312XX2TM': {
-        name: 'Modern Benoni',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Modern_Benoni_formation'
-      },
-      '112002XX': {
-        name: 'Closed Sicilian',
-        url: 'https://en.wikipedia.org/wiki/Pawn_structure#Closed_Sicilian_formation'
+    keySimilarity=(kSearch,kNamed)=>{
+      kSearch=kSearch.replaceAll(' ','');
+      kNamed=kNamed.replaceAll(' ','');
+      const l=Math.min(kSearch.length,kNamed.length);
+      let sim=0;
+      let prefixOver=false;
+      for (let i=0; i<l; i++) {
+        if (kSearch[i]==kNamed[i]) {
+          sim+=prefixOver?0.8:1;
+        } else {
+          prefixOver=true;
+          const pSearch=+kSearch[i];
+          const pNamed=+kNamed[i];
+          if (pSearch>=0 && pNamed>=0 && pSearch<pNamed) {
+            sim+=0.2;
+          }
+        }
       }
+      return Math.round(sim*100/l);
     };
 
-    keySimilarity=(k1,k2)=>{
-      let diff=0;
-      let p=1;
-      const l=k1.length;
-      for (let i=0; i<l; i++) {
-        if (k1[i]==k2[i]) continue;
-        const v='123'.includes(k1[i]) && '123'.includes(k2[i])
-          ? 0.5
-          : 1;
-        diff+=v*p;
-        p-=0.06;
-      }
-      return parseInt((l-diff)*100/l);
+    getOpposingStructure=structure=>{
+      const result =
+        structure.substr(3,3) +
+        structure.substr(0,3) +
+        structure.substr(6,2).replaceAll('M','x').replaceAll('T','M').replaceAll('x','T') +
+        ' ' +
+        structure.substr(12,3) +
+        structure.substr(9,3) +
+        ' ' +
+        structure.substr(18,2) +
+        structure.substr(16,2) +
+        ' ' +
+        structure.substr(24,3) +
+        structure.substr(21,3) +
+        ' ' +
+        structure.substr(31,3) +
+        structure.substr(28,3) +
+        ' ' +
+        structure.substr(37,2) +
+        structure.substr(35,2);        
+      return result;
     };
 
     getStructureName=(structure)=>{
       const parent=this.lichessTools;
-      const threshold=90;
-      const key=structure.split(' ')[0];
-      const name=this.names[key];
-      if (name) return name;
-      if (this.options.fuzzy) {
-        const arr=Object.keys(this.names).map(k=>{
+      const threshold=this.options.fuzzy?90:100;
+
+      const getArr=(structure)=>{
+        const arr=Object.keys(parent.pawnStructures).map(k=>{
           return {
             key:k, 
-            name:this.names[k],
-            similarity: this.keySimilarity(k,key)
+            value:parent.pawnStructures[k],
+            similarity: this.keySimilarity(structure,k)
           };
         });
         arr.sort((o1,o2)=>o2.similarity-o1.similarity);
-        if (arr[0].similarity>threshold&&arr[0].similarity!=arr[1].similarity) {
-          return arr[0].name;
-        }
+        return arr;
+      };
+
+      const arr=getArr(structure);
+      if (arr[0].similarity==100) return arr[0].value;
+      const opp=this.getOpposingStructure(structure);
+      const oppArr=getArr(opp).map(i=>{
+        return {
+          ...i,
+          value:{
+            ...i.value,
+            name: i.value.name+' (R)'
+          }
+        };
+      });
+      if (oppArr[0].similarity==100) return oppArr[0].value;
+
+      const arrItem=arr[0].similarity>oppArr[0].similarity
+        ? arr[0]
+        : oppArr[0];
+      
+      if (arrItem.similarity>=threshold) {
+        return arrItem.value;
       }
+      
       if (this.options.onlyNamed) return null;
-      return { name: key };
+      return { 
+        name: structure.split(' ')[0],
+        best: arrItem
+      };
     };
 
     addStructureAnchor=(el,structureName,structure)=>{
@@ -288,9 +257,11 @@
           structureElem.attr('target','_blank');
         }
       }
+      let title=trans.pluralSame('structureNameTitle',structure);
+      if (structureName.best) title+='\r\n'+structureName.best.value.name+' '+structureName.best.similarity+'%';
       structureElem
         .text(structureName.name)
-        .attr('title',trans.pluralSame('structureNameTitle',structure));
+        .attr('title',title);
       if (isLink) {
         structureElem.attr('href',structureName.url);
       }
@@ -329,7 +300,7 @@
       if (parent.global.document.hidden) return;
       if ($('body').is('.playing')) return;
       const trans=parent.translator;
-      const metaSection = $('div.game__meta section, div.analyse__wiki.empty, div.chat__members').eq(0);
+      const metaSection = $('div.game__meta section, div.analyse__wiki.empty, div.chat__members, div.analyse__underboard .copyables').eq(0);
       const fen=parent.getPositionFromBoard($('main'),true);
       const board=parent.getBoardFromFen(fen);
       const structure=this.getStructure(board,$('.cg-wrap').is('.orientation-black'));
