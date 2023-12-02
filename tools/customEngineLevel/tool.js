81196c291f49b1ca904c71c9ed317e602ef17be3
Siderite <Costin.Manda@gmail.com>
2023-12-02 14:58:25 +0200

    more custom engine tweaks

--- tools/customEngineLevel/tool.js
+++ tools/customEngineLevel/tool.js
@@ -130,13 +130,14 @@
 
       const node=analysis.ceval.lastStarted?.steps?.at(-1);
       const curDepth=node?.ceval?.depth;
-      if (analysis.ceval.showingCloud() && analysis.ceval.canGoDeeper()
-          && (this.options.noCloud || (this.options.depth && curDepth<this.options.depth)))
-      {
-        node.autoDeeper=true;
-        analysis.ceval.goDeeper();
-        analysis.redraw();
-        return;
+      if (analysis.ceval.canGoDeeper() && analysis.ceval.getState()==2) {
+        if ((analysis.ceval.showingCloud() && this.options.noCloud) || (this.options.depth && curDepth<(node.autoDeeper || this.options.depth) ))
+        {
+          node.autoDeeper=this.options.depth;
+          analysis.ceval.goDeeper();
+          analysis.redraw();
+          return;
+        }
       }
       if (!analysis.ceval.showingCloud() && (node.autoDeeper || !analysis.ceval.isDeeper())
           && this.options.depth && curDepth>=this.options.depth)
