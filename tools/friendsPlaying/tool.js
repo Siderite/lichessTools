e99a19136e12dc6aa8e695b747f52ec0e8e84014
Siderite <Costin.Manda@gmail.com>
2023-12-27 16:36:35 +0200

    Do not read current game if slow mode or silent in friends playing tool

--- tools/friendsPlaying/tool.js
+++ tools/friendsPlaying/tool.js
@@ -85,14 +85,16 @@
     let eventType='';
     let variant='';
     let text='';
-    try {
-      text = await this.lichessTools.net.fetch({url:'/api/games/user/{username}?max=1&tags=true&ongoing=true&finished=false',args:{username:username}});
-    } catch(e) {
-      if (e.toString().includes('Failed to fetch')) {
-        this.lichessTools.global.console.debug('Failed to fetch net error');
-        return;
+    if (!silent && !this.lichessTools.net.slowMode) {
+      try {
+        text = await this.lichessTools.net.fetch({url:'/api/games/user/{username}?max=1&tags=true&ongoing=true&finished=false',args:{username:username}});
+      } catch(e) {
+        if (e.toString().includes('Failed to fetch')) {
+          this.lichessTools.global.console.debug('Failed to fetch net error');
+          return;
+        }
+        this.lichessTools.global.console.warn('playFriendSound game fetch error',e);
       }
-      this.lichessTools.global.console.warn('playFriendSound game fetch error',e);
     }
     if (text) {
       gameType=this.lichessTools.getGameTime(this.lichessTools.getPgnTag(text,'TimeControl'));
