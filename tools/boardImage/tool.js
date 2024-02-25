fc10f1559d276fa9c5bd1f49769686893fe4842b
Siderite <Costin.Manda@gmail.com>
2024-02-25 23:31:17 +0200

    Using dialog as it should be used

--- tools/boardImage/tool.js
+++ tools/boardImage/tool.js
@@ -72,25 +72,21 @@
         const img=await this.drawSvg(e);
         ctx.drawImage(img,0,0);
       });
+      $('dialog.lichessTools-boardImage').remove();
       const dialog=$('<dialog class="lichessTools-boardImage">')
-        .append(`
+        .append(`<form>
     <div class="close-button-anchor">
         <a class="help-button" data-icon="&#xE005;" aria-label="Help" href="https://siderite.dev/blog/lichess-tools---user-manual#boardImage" target="_blank"></a>
-        <button class="close-button" data-icon="&#xE03F;" aria-label="Close"/>
+        <button class="close-button" data-icon="&#xE03F;" aria-label="Close" formmethod="dialog" value="close"/>
     </div>
     <div class="scrollable">
         <div class="dialog-content">
         </div>
-    </div>`)
+    </div>
+</form>`)
         .appendTo('body');
       $('dialog .dialog-content').append(canvas);
-      $('button.close-button',dialog)
-        .on('click',ev=>{
-          ev.preventDefault();
-          $('body').off('keydown',this.closeDialog);
-          this.closeDialog();
-        });
-      $('body').on('keydown',this.closeDialog);
+      dialog[0].showModal();
     };
 
     closeDialog=(ev)=>{
