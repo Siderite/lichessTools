ed6ef80af10cae5e8a165aa2e79f56c1d99ce731
Siderite <Costin.Manda@gmail.com>
2024-04-23 21:49:12 +0300

    Decimals in the move list https://github.com/Siderite/lichessTools/issues/532

--- tools/cevalDecimals/tool.js
+++ tools/cevalDecimals/tool.js
@@ -41,23 +41,46 @@
       if (!analysis) return;
       const trans=parent.translator;
       const ceval=analysis.node.ceval;
-      if (!ceval) return;
-      const pearl=$('div.ceval.enabled pearl');
-      if (pearl.length) {
-        // lichess keeps a reference to the actual node
-        const textNode = Array.from(pearl[0].childNodes).find(n=>n.nodeType==3);
-        const text=this.renderEval(ceval.cp,ceval.mate);
-        if (textNode&&text) textNode.textContent=text;
+      if (ceval) {
+        const pearl=$('div.ceval.enabled pearl');
+        if (pearl.length) {
+          // lichess keeps a reference to the actual node
+          const textNode = Array.from(pearl[0].childNodes).find(n=>n.nodeType==3);
+          const text=this.renderEval(ceval.cp,ceval.mate);
+          if (textNode&&text) textNode.textContent=text;
+        }
+        $('div.ceval.enabled ~ div.pv_box')
+          .find('div.pv[data-uci]')
+          .each((i,e)=>{
+            const uci=$(e).attr('data-uci');
+            const pv=ceval.pvs.find(p=>p.moves?.at(0)===uci);
+            if (pv) {
+              $('strong',e).text(this.renderEval(pv.cp,pv.mate));
+            }
+          });
       }
-      $('div.ceval.enabled ~ div.pv_box')
-        .find('div.pv[data-uci]')
-        .each((i,e)=>{
-          const uci=$(e).attr('data-uci');
-          const pv=ceval.pvs.find(p=>p.moves?.at(0)===uci);
-          if (pv) {
-            $('strong',e).text(this.renderEval(pv.cp,pv.mate));
+      const traverse=(node,path)=>{
+        let evl=node.eval;
+        const ceval=node.ceval;
+        if (!evl || ceval?.depth>=16) evl=ceval;
+        if (evl) {
+          const elem=parent.getElementForPath(path);
+          if (elem) {
+            let evalElem=$('eval',elem);
+            if (!evalElem?.length) {
+              evalElem=$('<eval class="lichessTools-cevalDecimals">').appendTo(elem);
+            }
+            const text=this.renderEval(evl.cp,evl.mate);
+            if (evalElem.text()!=text) {
+              evalElem.text(text);
+            }
           }
-        });
+        }
+        for (const child of node.children||[]) {
+           traverse(child,path+child.id);
+        }
+      };
+      traverse(analysis.tree.root,'');
     };
 
     async start() {
