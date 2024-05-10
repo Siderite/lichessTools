8cf0b0bbb53ba92078ae86d06539b3eb4deeb846
Siderite <Costin.Manda@gmail.com>
2024-05-10 22:02:57 +0300

    Apply layout even when the control changes https://github.com/Siderite/lichessTools/issues/564

--- tools/playLayout/tool.js
+++ tools/playLayout/tool.js
@@ -48,25 +48,46 @@
       }
     };
 
-    async start() {
+    applyLayout=()=>{
       const parent=this.lichessTools;
       const lichess=parent.lichess;
       const $=parent.$;
       const trans=parent.translator;
-      const value=parent.currentOptions.getValue('playLayout');
-      this.options={ value: value };
-      this.logOption('Play layout', value);
-      $('body').toggleClass('lichessTools-noSide',value=='noSide');
-      $('body').toggleClass('lichessTools-smallSide',value=='smallSide');
       $('main.round .game__meta__infos')
         .removeClass('lichessTools-pointer')
+        .removeAttr('title')
         .off('click',this.toggleLayout);
-      if (value!='normal') {
+      if (this.options.value!='normal') {
         $('main.round .game__meta__infos')
           .addClass('lichessTools-pointer')
           .attr('title',trans('toggleLayoutTitle'))
           .on('click',this.toggleLayout);
       }
+    };
+
+    async start() {
+      const parent=this.lichessTools;
+      const lichess=parent.lichess;
+      const $=parent.$;
+      const value=parent.currentOptions.getValue('playLayout');
+      this.options={ value: value };
+      this.logOption('Play layout', value);
+      this._control = $('main.round .game__meta__infos')[0];
+      if (!this._control) return;
+      $('body')
+        .toggleClass('lichessTools-noSide',value=='noSide')
+        .toggleClass('lichessTools-smallSide',value=='smallSide');
+      parent.global.clearInterval(this.interval);
+      if (value!='normal') {
+        this.interval = parent.global.setInterval(()=>{
+          const control=$('main.round .game__meta__infos')[0];
+          if (this._control != control) {
+            this._control = control;
+            this.applyLayout();
+          }
+        },1000);
+      }
+      this.applyLayout();
     }
 
   }
