3cf326677fe780541ed902eee66bb85610df2e53
Siderite <Costin.Manda@gmail.com>
2024-07-13 21:52:44 +0300

    Add auto expand functionality https://github.com/Siderite/lichessTools/issues/684

--- tools/expandAll/tool.js
+++ tools/expandAll/tool.js
@@ -7,8 +7,8 @@
       {
         name:'expandAll',
         category: 'analysis',
-        type:'single',
-        possibleValues: [false,true],
+        type:'multiple',
+        possibleValues: ['showButton','autoExpand'],
         defaultValue: false,
         advanced: true
       }
@@ -18,11 +18,15 @@
       'en-US':{
         'options.analysis': 'Analysis',
         'options.expandAll': 'Expand all variations button',
+        'expandAll.showButton': 'Show button',
+        'expandAll.autoExpand': 'Auto expand',
         'expandAllTitle':'LiChess Tools - expand all variations'
       },
       'ro-RO':{
         'options.analysis': 'Analiz\u0103',
         'options.expandAll': 'Buton expandare toate varia\u0163iunile',
+        'expandAll.showButton': 'Arat\u0103 butonul',
+        'expandAll.autoExpand': 'Expandeaz\u0103 automat',
         'expandAllTitle':'LiChess Tools - expandeaz\u0103 toate varia\u0163iunile'
       }
     }
@@ -31,39 +35,65 @@
       const parent=this.lichessTools;
       const $=parent.$;
       const trans=parent.translator;
-      const analysis=parent.lichess.analysis;
-      const button=$('button.lichessTools-expandAll');
+      let button=$('button.lichessTools-expandAll');
       if ($('.tview2 line.expand').length) {
         if (button.length) return;
       } else {
         button.remove();
         return;
       }
-      $('<button type="button" class="lichessTools-expandAll">')
+      button=$('<button type="button" class="lichessTools-expandAll">')
         .attr('data-icon','\uE02F')
         .attr('title',trans.noarg('expandAllTitle'))
         .on('mousedown touchstart',ev=>{
           ev.preventDefault();
           ev.stopPropagation();
-          analysis.setAllCollapsed('',false);
-          button.remove();
+          this.expandAll();
         })
         .insertBefore($('index.sbhint1').eq(0));
     };
+
+    expandAll=()=>{
+      const parent=this.lichessTools;
+      const $=parent.$;
+      const analysis=parent.lichess.analysis;
+      analysis.setAllCollapsed('',false);
+      $('button.lichessTools-expandAll').remove();
+    }
+
+    autoExpand=()=>{
+      const parent=this.lichessTools;
+      const $=parent.$;
+      const tview2=$('.tview2')[0];
+      if (!tview2 || tview2.autoExpanded) return;
+      this.expandAll();
+      tview2.autoExpanded=true;
+    }
 
     async start() {
       const parent=this.lichessTools;
       const value=parent.currentOptions.getValue('expandAll');
       this.logOption('Expand all', value);
+      this.options={
+        showButton:parent.isOptionSet(value,'showButton'),
+        autoExpand:parent.isOptionSet(value,'autoExpand'),
+        get isSet() { return this.showButton || this.autoExpand; }
+      };
       const lichess=parent.lichess;
       const $=parent.$;
       const analysis=lichess?.analysis;
       if (!analysis) return;
       $('button.lichessTools-expandAll').remove();
       lichess.pubsub.off('redraw',this.addExpandAllButton);
-      if (!value) return;
-      lichess.pubsub.on('redraw',this.addExpandAllButton);
-      this.addExpandAllButton();
+      lichess.pubsub.off('redraw',this.autoExpand);
+      if (this.options.showButton) {
+        lichess.pubsub.on('redraw',this.addExpandAllButton);
+        this.addExpandAllButton();
+      }
+      if (this.options.autoExpand) {
+        lichess.pubsub.on('redraw',this.autoExpand);
+        this.autoExpand();
+      }
     }
 
   }
