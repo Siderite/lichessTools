999114fb44ddd4778389b8e52aae2e224a8dd8d4
Siderite <Costin.Manda@gmail.com>
2024-09-15 20:25:38 +0300

    Get rid of FOUC in Firefox https://github.com/Siderite/lichessTools/issues/809

--- scripts/lichessTools-start.js
+++ scripts/lichessTools-start.js
@@ -1,7 +1,8 @@
-const start = () => setTimeout(() => lichessTools.start(window.site));
+const start = () => window.setTimeout(() => lichessTools.start(window.site),100);
 const load = window.site?.load;
 if (load) {
   load.then(start);
 } else {
-  window.setTimeout(start, 100);
+  start();
 }
+
