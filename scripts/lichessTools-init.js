872cfc812c0dc7351237ad59cb64976b78a01c0a
Siderite <Costin.Manda@gmail.com>
2024-10-27 09:57:20 +0200

    remove fixCeval tool because it never worked. The cause for the freeze must be determined and it probably has to do with LiChess Tools :(

--- scripts/lichessTools-init.js
+++ scripts/lichessTools-init.js
@@ -11,7 +11,6 @@ lichessTools.loadTool(LiChessTools.Tools.InterceptEventHandlers);
 lichessTools.loadTool(LiChessTools.Tools.FixAbortController);
 lichessTools.loadTool(LiChessTools.Tools.FixChessgroundResize);
 lichessTools.loadTool(LiChessTools.Tools.FixWakeLock);
-lichessTools.loadTool(LiChessTools.Tools.FixCeval);
 lichessTools.loadTool(LiChessTools.Tools.EmitChapterChange);
 lichessTools.loadTool(LiChessTools.Tools.GamebookPlayClass);
 lichessTools.loadTool(LiChessTools.Tools.EmitPuzzleChange);
