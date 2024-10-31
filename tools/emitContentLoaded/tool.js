1df0a355083312bedb0971606daa21f941990460
Siderite <Costin.Manda@gmail.com>
2024-10-31 18:02:18 +0200

    Translation updates

--- tools/emitContentLoaded/tool.js
+++ tools/emitContentLoaded/tool.js
@@ -12,14 +12,25 @@
     };
     emit = this.lichessTools.debounce(this.emitDirect,250);
 
+    detectNew = (records)=>{
+      const lt = this.lichessTools;
+      const $ = lt.$;
+      if (records.find(r=>r.addedNodes.find(n=>$(n).is('.paginated')))) {
+        this.emit();
+      }
+    };
+
     async start() {
       const lt = this.lichessTools;
       const lichess = lt.lichess;
       const $ = lt.$;
-      if (lichess.pubsub) {
+      /*if (lichess.pubsub) {
         lichess.pubsub.off('content-loaded',this.emit);
         lichess.pubsub.on('content-loaded',this.emit);
-      }
+      }*/
+      const observer = $('body').observer();
+      observer.clear();
+      observer.on('.infinite-scroll',this.detectNew);
     }
 
   }
