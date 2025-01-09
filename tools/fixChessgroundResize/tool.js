9a4ae8499a6a3fd0350e880b072ef7ad34b7b860
Siderite <Costin.Manda@gmail.com>
2025-01-09 23:36:38 +0200

    fix mobile theme move list height for strange resolutions

--- tools/fixChessgroundResize/tool.js
+++ tools/fixChessgroundResize/tool.js
@@ -36,6 +36,8 @@
       if (JSON.stringify(offset) != JSON.stringify(this.offset)) {
         this.offset = offset;
         lt.debug && lt.global.console.debug('Firing board resize event');
+        const boardSize = $('cg-container').css('width') || $('cg-container').width()+'px';
+        lt.global.document.documentElement.style.setProperty('--board-size', boardSize);
         $('body').trigger('resize');
       }
     }
