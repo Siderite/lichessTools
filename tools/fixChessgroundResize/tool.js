ebf75947924217a94babf1a0afcc411eb5e2a9e7
Siderite <Costin.Manda@gmail.com>
2025-01-10 16:06:51 +0200

    set CSS variable only for main board

--- tools/fixChessgroundResize/tool.js
+++ tools/fixChessgroundResize/tool.js
@@ -36,7 +36,7 @@
       if (JSON.stringify(offset) != JSON.stringify(this.offset)) {
         this.offset = offset;
         lt.debug && lt.global.console.debug('Firing board resize event');
-        const boardSize = $('cg-container').css('width') || $('cg-container').width()+'px';
+        const boardSize = $('.main-board cg-container').css('width') || $('.main-board cg-container').width()+'px';
         lt.global.document.documentElement.style.setProperty('--board-size', boardSize);
         $('body').trigger('resize');
       }
