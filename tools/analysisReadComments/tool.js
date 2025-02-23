72e2a3ef3488c8f0654e519b426adbc4c8279829
Siderite <Costin.Manda@gmail.com>
2025-02-23 13:14:25 +0200

    Options for comment reading https://github.com/Siderite/lichessTools/issues/1040

--- tools/analysisReadComments/tool.js
+++ tools/analysisReadComments/tool.js
@@ -5,8 +5,8 @@
       {
         name: 'analysisReadComments',
         category: 'analysis2',
-        type: 'single',
-        possibleValues: [false, true],
+        type: 'multiple',
+        possibleValues: ['enabled', 'stripEmoji', 'readAnnotations'],
         defaultValue: false,
         advanced: true
       }
@@ -16,11 +16,17 @@
       'en-US': {
         'options.analysis2': 'Analysis - minor',
         'options.analysisReadComments': 'Read move comments',
+        'analysisReadComments.enabled': 'Enabled',
+        'analysisReadComments.stripEmoji': 'Ignore emojis',
+        'analysisReadComments.readAnnotations': 'Read annotations',
         'readCommentsButtonTitle': 'LiChess Tools - toggle comment reading'
       },
       'ro-RO': {
         'options.analysis2': 'Analiz\u0103 - m\u0103run\u0163i\u015furi',
         'options.analysisReadComments': 'Cite\u015fte comentarii mut\u0103ri',
+        'analysisReadComments.enabled': 'Activat',
+        'analysisReadComments.stripEmoji': 'Ignor\u0103 emoji',
+        'analysisReadComments.readAnnotations': 'Cite\u0157te adnot\u0103ri',
         'readCommentsButtonTitle': 'LiChess Tools - comut\u0103 citire comentarii'
       }
     }
@@ -46,7 +52,9 @@
     getSpeakableText = (text)=>{
       if (!text) return;
       text = text.replaceAll(/(cls|bkm|prc|rnd):([^\s]*)\s*/gi,'');
-      text = text.replaceAll(/\p{Extended_Pictographic}+/ugi,' emoji ');
+      if (this.options.stripEmoji) {
+        text = text.replaceAll(/\p{Extended_Pictographic}+/ugi,' ');
+      }
       text = text.replaceAll(/e\.\s*p\./gi,'un phsaant');
       text = text.replaceAll(this.urlRegex,(m)=>{
         const url = new URL(m);
@@ -110,11 +118,19 @@
       const node = lichess.analysis.node;
       if (!node) return;
       const comments = lt.getNodeCommentsText(node);
-      if (comments === this.prevComments) return;
       this.prevComments = comments;
-      if (!comments) return;
-      const speakable = this.getSpeakableText(comments);
-      lt.speak(speakable, { rate: 1.25 });
+      let speakable = this.getSpeakableText(comments);
+      if (this.options.readAnnotations && node.glyphs?.length) {
+        speakable = node.glyphs
+          .map(g=>g.name || this.annotations[g.symbol])
+          .concat([speakable])
+          .filter(g=>g)
+          .join(', ');
+      }
+      if (speakable?.trim() && speakable != this.prevSpeakable) {
+        lt.speak(speakable, { rate: 1.25 });
+      }
+      this.prevSpeakable = speakable;
     }
 
     showInteractiveButton = () => {
@@ -150,7 +166,11 @@
       const $ = lt.$;
       const value = lt.currentOptions.getValue('analysisReadComments');
       this.logOption('Analysis read comments', value);
-      this.options = { enabled: !!value };
+      this.options = {
+        enabled: lt.isOptionSet(value, 'enabled'),
+        stripEmoji: lt.isOptionSet(value, 'stripEmoji'),
+        readAnnotations: lt.isOptionSet(value, 'readAnnotations')
+      };
 
       if (!lichess?.analysis || !lt.uiApi) return;
       const isRelay = lichess.analysis.study?.relay;
