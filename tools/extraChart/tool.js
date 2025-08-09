86186930c28164c6f23a7fe3a68e770e09897918
Siderite <Costin.Manda@gmail.com>
2025-08-09 10:59:22 +0300

    Random tweaks to the brilliant algorithm. Perhaps  I should start from scratch...

--- tools/extraChart/tool.js
+++ tools/extraChart/tool.js
@@ -683,7 +683,7 @@
       const mmw1 = this.maxMaterialWon(board, side) / 100;
       board = lt.getBoardFromFen(prev2Node.fen);
       const mmw3 = this.maxMaterialWon(board, side) / 100;
-      const bril = (mmw3 - mmw1) - delta; // TODO thesis: brilliant if there is a lot of material loss but still a good move
+      const bril = ((mmw3 - mmw1) * side - delta)*0.7; // TODO thesis: brilliant if there is a lot of material loss but still a good move
       return bril + bonus;
     };
 
