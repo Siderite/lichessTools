a0ecc70bfc40dff8acf56921bc5d56c1476121e0
Siderite <Costin.Manda@gmail.com>
2025-10-14 18:45:55 +0300

    Updating image pasting to use ImgBB

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -39,26 +39,60 @@ const handlers = {
     const response = await obs.call('GetSceneList').catch(e => { console.debug('Could not get scenes', data); });
     return { sceneNames: response.scenes.map(s => s.sceneName) };
   },
-  pasteBuffer: async (data) => {
+  pasteBuffer: async(data) => {
     const buffer = data?.options?.buffer;
-    if (!buffer) return;
-    const formData = new FormData();
-    formData.append('image', buffer);
-    formData.append('type', 'base64');
-    formData.append('title', 'LiChess Tools image');
-    formData.append('description', 'from Inbox chat paste in Lichess.org');
-    const response = await fetch(
-      'https://api.imgur.com/3/image',
-      {
-        method: 'POST',
-        headers: new Headers({
-          Authorization: 'Client-ID dd38da6dc32098d',
-        }),
-        body: formData
-      });
-    if (!response.ok) throw new Error('Imgur upload failed with status ' + response.status);
-    const responseData = await response.json();
-    return { link: responseData.data?.link };
+    if (!buffer)
+      return;
+    const hostingService = 'imgbb';
+    switch (hostingService) {
+      case 'imgur': {
+        const formData = new FormData();
+        formData.append('image', buffer);
+        formData.append('type', 'base64');
+        formData.append('title', 'LiChess Tools image');
+        formData.append('description', 'from Inbox chat paste in Lichess.org');
+        const response = await fetch(
+            'https://api.imgur.com/3/image', {
+            method: 'POST',
+            headers: new Headers({
+              Authorization: 'Client-ID dd38da6dc32098d',
+            }),
+            body: formData
+          });
+        if (!response.ok)
+          throw new Error('Imgur upload failed with status ' + response.status);
+        const responseData = await response.json();
+        return {
+          link: responseData.data?.link
+        };
+      }
+      break;
+      case 'imgbb': { 
+        const formData = new FormData();
+        formData.append('image', buffer);
+        formData.append('type', 'base64');
+        formData.append('title', 'LiChess Tools image');
+        formData.append('description', 'from Inbox chat paste in Lichess.org');
+        let url = 'https://api.imgbb.com/1/upload?expiration={expiration}&key={key}';
+        const args = { expiration:86400*100, key:'370e6c2c08e7773fc0960160e36f535c' };
+        for (const k in args) {
+          url = url.replace('{' + k + '}', encodeURIComponent(args[k]));
+        }
+        const response = await fetch(url, {
+            method: 'POST',
+            body: formData
+          });
+        if (!response.ok)
+          throw new Error('Imgbb upload failed with status ' + response.status);
+        const responseData = await response.json();
+        return {
+          link : responseData.data?.url
+        };
+      }
+      break;
+      default:
+        throw new Error('Image hosting service ' + hostingService + ' not supported!');
+    }
   },
   getFile: async (data) => {
     const filename = data?.options?.filename;
