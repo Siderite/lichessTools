57397e5d0885210a99f24a62c780c64538bd1d7c
Siderite <Costin.Manda@gmail.com>
2025-10-27 15:51:57 +0200

    Simplify boardStyle

--- tools/boardStyle/tool.js
+++ tools/boardStyle/tool.js
@@ -23,13 +23,22 @@
       }
     }
 
+    getSquarePosition = (square) => {
+      const transform = square.style?.transform;
+      const m = /translate\(\s*(?<x>\d+(?:\.\d+)?)px\s*,\s*(?<y>\d+(?:\.\d+)?)px\s*\)/.exec(transform);
+      if (!m) return null;
+      const position = { left: +m.groups.x, top: +m.groups.y };
+      return position;
+    };
+
     processBoards = ()=>{
       const lt = this.lichessTools;
       const $ = lt.$;
       $('cg-container').each((i,e)=>{
         const container = $(e);
-        const containerRect = e.getBoundingClientRect();
-        const q = 800/containerRect.width;
+        const containerWidth = e.clientWidth;
+        if (!containerWidth) return;
+        const q = 800/containerWidth;
         container.toggleClassSafe('lichessTools-boardStyle',true);
 
         const selectedKey = container.find('square.selected').prop('cgKey');
@@ -63,35 +72,44 @@
           const isBlackTurn = cgPiece[0]=='white';
           const lastMovedPiece = cgPiece[1];
 
-          const pos = lastMoves
-                            .map(s=>s.getBoundingClientRect())
-                            .map(r=>({ x:q*(r.x+r.width/2-containerRect.x), y:q*(r.y+r.height/2-containerRect.y) }));
-          const dest = pos[0];
-          const orig = pos[1];
-          if (!arrow.length) {
-            arrow = $(`<svg class="lichessTools-lastMoveArrow" viewBox="0 0 800 800">
-      <defs>
-        <linearGradient id="arrowGradient" gradientUnits="userSpaceOnUse">
-          <stop offset="0%" class="stop-0" />
-          <stop offset="100%" class="stop-100" />
-        </linearGradient>
-        <marker id="arrowhead" markerWidth="5" markerHeight="3.5" refX="3" refY="1.75" orient="auto" markerUnits="strokeWidth">
-          <polygon points="0 0, 5 1.75, 0 3.5" class="arrowhead" />
-        </marker>
-      </defs>
-      <line class="arrow-line" />
-    </svg>`).appendTo(container);
+          if (e.usesArrow !== false)
+          {
+            const halfSquare = containerWidth/16;
+            const pos = lastMoves
+                            .map(s=>this.getSquarePosition(s))
+                            .filter(r=>r)
+                            .map(r=>({ x:q*(r.left+halfSquare), y:q*(r.top+halfSquare) }));
+            if (pos.length!=2) return;
+            const dest = pos[0];
+            const orig = pos[1];
+            if (!arrow.length) {
+              arrow = $(`<svg class="lichessTools-lastMoveArrow" viewBox="0 0 800 800">
+        <defs>
+          <linearGradient id="arrowGradient" gradientUnits="userSpaceOnUse">
+            <stop offset="0%" class="stop-0" />
+            <stop offset="100%" class="stop-100" />
+          </linearGradient>
+          <marker id="arrowhead" markerWidth="5" markerHeight="3.5" refX="3" refY="1.75" orient="auto" markerUnits="strokeWidth">
+            <polygon points="0 0, 5 1.75, 0 3.5" class="arrowhead" />
+          </marker>
+        </defs>
+        <line class="arrow-line" />
+      </svg>`).appendTo(container);
+            }
+            arrow.find('.arrow-line')
+              .attrSafe('x1',orig.x)
+              .attrSafe('y1',orig.y)
+              .attrSafe('x2',dest.x)
+              .attrSafe('y2',dest.y);
+            arrow.find('#arrowGradient')
+              .attrSafe('x1',orig.x)
+              .attrSafe('y1',orig.y)
+              .attrSafe('x2',dest.x)
+             .attrSafe('y2',dest.y);
           }
-          arrow.find('.arrow-line')
-            .attrSafe('x1',orig.x)
-            .attrSafe('y1',orig.y)
-            .attrSafe('x2',dest.x)
-            .attrSafe('y2',dest.y);
-          arrow.find('#arrowGradient')
-            .attrSafe('x1',orig.x)
-            .attrSafe('y1',orig.y)
-            .attrSafe('x2',dest.x)
-            .attrSafe('y2',dest.y);
+          if (e.usesArrow === undefined) {
+            e.usesArrow = arrow.css('display') != 'none';
+          }
           const side = isBlackTurn ? 'black' : 'white';
           const prevSide = container.attr('sideToPlay');
           const prevDestKey = container.attr('lastMoveSquare');
