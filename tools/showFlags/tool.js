399344b3a2ef785e558273331223c5c1e390cfaf
Siderite <Costin.Manda@gmail.com>
2026-02-23 15:11:55 +0200

    Fix small issues after Lichess changes

--- tools/showFlags/tool.js
+++ tools/showFlags/tool.js
@@ -29,7 +29,7 @@
       const $ = lt.$;
       const dict = {};
       $.cached('.user-link,a[href^="/@/"]', 2000).each((i, e) => {
-        if ($(e).closest('#friend_box,.lichessTools-onlineFriends,div.complete-list,.crosstable__users,div.chat__members,#dasher_app,.lichessTools-challengeOptions,#topnav').length) return;
+        if ($(e).closest('#friend_box,.lichessTools-onlineFriends,div.complete-list,.crosstable__users,div.chat__members,#dasher_app,.lichessTools-challengeOptions,#topnav,.advice-summary__player').length) return;
 
         let textEl = $('.text', e);
         if (!textEl.length) textEl = $(e);
