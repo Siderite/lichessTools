6c5a840871ae6a9ac62cc59be835b8be1e90cbdc
Siderite <Costin.Manda@gmail.com>
2026-03-16 12:02:28 +0200

    Fix console site age colors

--- scripts/lichessTools.js
+++ scripts/lichessTools.js
@@ -2763,8 +2763,8 @@
         : 0;
       let style = '';
       if (age<7) {
-        const background = this.getGradientColor(age, [{ q: 2, color: '#FF0000' }, { q: 5, color: '#FFFF00' }, { q: 8, color: '#808080' }]);
-        const text = this.getGradientColor(age, [{ q: 2, color: '#FFFFFF' }, { q: 5, color: '#000000' }]);
+        const background = this.getGradientColor(age, [{ q: 0, color: '#FF0000' }, { q: 2, color: '#FF8000' }, { q: 5, color: '#FFFF00' }, { q: 8, color: '#808080' }]);
+        const text = this.getGradientColor(age, [{ q: 0, color: '#FFFFFF' }, { q: 5, color: '#000000' }]);
         style = 'background:'+background+'; color:'+text+';';
       }
       this.global.console.debug('%c site code age: ' + Math.round(age * 10) / 10 + ' days', style);
