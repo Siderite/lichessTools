3b8f623e541f66db57afa406b3c739b5948ff4ec
Siderite <Costin.Manda@gmail.com>
2026-09-06 20:31:40 +0300

    Update to Stockfish 19

--- tools/stockfish/tool.js
+++ tools/stockfish/tool.js
@@ -93,11 +93,11 @@
         useBetterEngine=this.lt.storage.supportsDb && (await this.lt.getMemorySize()) >= 4;
       }
       if (useBetterEngine) {
-        engineId = '__sf_18';
-        engineRoot = 'sf_18_relaxed-simd.js';
+        engineId = '__sf_19';
+        engineRoot = 'sf_19_relaxed-simd.js';
       } else {
-        engineId = '__sf_18_smallnet';
-        engineRoot = 'sf_18_smallnet_relaxed-simd.js';
+        engineId = '__sf_19_smallnet';
+        engineRoot = 'sf_19_smallnet_relaxed-simd.js';
       }
       try {
         if (!this._module) {
