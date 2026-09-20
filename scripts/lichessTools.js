0be409d5e63cd6891967c3cad7eaeef23efeb7d5
Siderite <Costin.Manda@gmail.com>
2026-09-20 09:33:12 +0300

    Use Lichess' speech.rate if not set https://github.com/Siderite/lichessTools/issues/1824

--- scripts/lichessTools.js
+++ scripts/lichessTools.js
@@ -1440,7 +1440,7 @@
     };
 
     speechVolume = 0.7;
-    speechRate = 1;
+    speechRate = null;
     speechVoiceIndex = undefined;
     speak = async (text, options) => {
       let volume = +options?.volume;
@@ -1453,7 +1453,7 @@
         volume: volume,
         voiceIndex: options?.voiceIndex === undefined ? this.speechVoiceIndex : options.voiceIndex,
         translated: !!options?.translated,
-        rate: options?.rate || this.speechRate
+        rate: options?.rate || this.speechRate || +this.storage.get('speech.rate') || 1;
       };
       const console = this.global.console;
       try {
